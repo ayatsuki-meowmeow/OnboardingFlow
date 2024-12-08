9108d01b2edc98684659e572038be6b145155a23
kono-meowmeow <aya.ayatsuki@gmail.com>
2024-12-08 11:21:53 +0900

    using border and set RoundedRectangle

--- OnboardingFlow/WelcomePage.swift
+++ OnboardingFlow/WelcomePage.swift
@@ -7,7 +7,30 @@ import SwiftUI
 
 struct WelcomePage: View {
     var body: some View {
-        Text("Welcome to MyAPP").font(.title).fontWeight(.bold).fontDesign(.serif)
+        VStack {
+            RoundedRectangle(cornerRadius: 30)
+                .frame(
+                    width: 150,
+                    height: 150
+                )
+//               すべてのアプリにはアクセントカラーがあり、デフォルトは青です。 アプリのアクセントカラーには、.tintスタイルを使用してアクセスします。
+                .foregroundStyle(.tint)
+            
+            Text("Welcome to MyAPP")
+                .font(.title)
+                .fontWeight(.bold)
+                .fontDesign(.serif)
+                .border(.black, width: 1.5)
+            
+            Text("これは勉強用に作っているアプリです。Swiftのチュートリアルを進めています。")
+                .font(.title2)
+                .multilineTextAlignment(.leading)
+                .border(.black, width: 1.5)
+                
+        }
+        .border(.orange, width: 1.5)
+        .padding()
+        .border(.blue, width: 1.5)
     }
 }
 
