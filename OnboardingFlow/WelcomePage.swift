102dbfc0eee8bf23f786b3e360e88090ef2af873
kono-meowmeow <aya.ayatsuki@gmail.com>
2024-12-08 11:07:38 +0900

    create welcome page

---
+++ OnboardingFlow/WelcomePage.swift
@@ -0,0 +1,16 @@
+//
+//  WelcomePage.swift
+//  OnboardingFlow
+//
+
+import SwiftUI
+
+struct WelcomePage: View {
+    var body: some View {
+        Text("Welcome to MyAPP").font(.title).fontWeight(.bold).fontDesign(.serif)
+    }
+}
+
+#Preview {
+    WelcomePage()
+}
