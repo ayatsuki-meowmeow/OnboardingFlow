693a8699a55b53296919731b8d1db4548e70367e
kono-meowmeow <aya.ayatsuki@gmail.com>
2024-12-08 11:50:12 +0900

    complete welcome page design

--- OnboardingFlow/WelcomePage.swift
+++ OnboardingFlow/WelcomePage.swift
@@ -8,13 +8,19 @@ import SwiftUI
 struct WelcomePage: View {
     var body: some View {
         VStack {
-            RoundedRectangle(cornerRadius: 30)
-                .frame(
-                    width: 150,
-                    height: 150
-                )
-//               すべてのアプリにはアクセントカラーがあり、デフォルトは青です。 アプリのアクセントカラーには、.tintスタイルを使用してアクセスします。
-                .foregroundStyle(.tint)
+            ZStack {
+                RoundedRectangle(cornerRadius: 30)
+                    .frame(
+                        width: 150,
+                        height: 150
+                    )
+                //               すべてのアプリにはアクセントカラーがあり、デフォルトは青です。 アプリのアクセントカラーには、.tintスタイルを使用してアクセスします。
+                    .foregroundStyle(.tint)
+                
+                Image(systemName: "figure.2.and.child.holdinghands")
+                    .font(Font.system(size: 70))
+                    .foregroundStyle(Color.white)
+            }
             
             Text("Welcome to MyAPP")
                 .font(.title)
@@ -23,17 +29,13 @@ struct WelcomePage: View {
                 .padding(Edge.Set.top)
                 .padding(Edge.Set.all)
                 .padding(Edge.Set.bottom)
-                .border(.black, width: 1.5)
             
             Text("これは勉強用に作っているアプリです。Swiftのチュートリアルを進めています。")
                 .font(.title2)
                 .multilineTextAlignment(.leading)
-                .border(.black, width: 1.5)
                 
         }
-        .border(.orange, width: 1.5)
         .padding()
-        .border(.blue, width: 1.5)
     }
 }
 
