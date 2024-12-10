128f6af6f02245c03c5babf091417b896ba05635
kono-meowmeow <aya.ayatsuki@gmail.com>
2024-12-10 21:16:54 +0900

    apply background color

--- OnboardingFlow/ContentView.swift
+++ OnboardingFlow/ContentView.swift
@@ -5,12 +5,22 @@
 
 import SwiftUI
 
+let gradientColors: [Color] = [
+    .gradientTop,
+    .gradientBottom
+]
+
 struct ContentView: View {
     var body: some View {
         TabView {
             WelcomePage()
             FeaturesPage()
         }
+        .background(
+            Gradient(
+                colors: gradientColors
+            )
+        )
         .tabViewStyle(
             PageTabViewStyle.page
         )
